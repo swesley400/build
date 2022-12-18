00d75a5f743bd11e303e96e003373f161dbb95c7
Wesley Vieira Santos <79053520+swesley400@users.noreply.github.com>
2022-12-18 11:27:39 -0300

    Test

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
 
+const port = process.env.PORT || 8080
+
 app.use('/home', express.static(__dirname + '/index.html'));
 
-app.listen(8080, ()=>{
-    console.log("Servidor Iniciado")
+
+
+
+app.listen(port, ()=>{
+
+    console.log("Servidor Iniciado", port)
 })
