122e34960cea0fa25ed898ca39d235193a0e715c
Wesley Vieira Santos <79053520+swesley400@users.noreply.github.com>
2022-12-18 11:32:40 -0300

    Test

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const port = process.env.PORT || 8080
 
 app.use('/home', express.static(__dirname + '/index.html'));
 
-
-
+app.get("/",(req, res) =>{
+    res.json({
+        message: "Hello Word"
+    })
+})
 
 app.listen(port, ()=>{
 
